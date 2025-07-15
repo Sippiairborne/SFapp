bdabba1eea3ea3c9749424fe1d11edde1d368d48
Sippiairborne <claimsmasters0@gmail.com>
2025-07-15 12:23:29 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'StormForce API is active' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`StormForce server running on port ${PORT}`);
+});
